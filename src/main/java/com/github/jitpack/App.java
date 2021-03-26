ea8d8e11ef86c4253d4580dae946a700519f5925
vamsinalluri <31075388+vamsinalluri@users.noreply.github.com>
2021-03-26 16:29:11 +0000

    Update App.java

--- src/main/java/com/github/jitpack/App.java
+++ src/main/java/com/github/jitpack/App.java
@@ -9,6 +9,34 @@ public class App
     public static void main(String[] args)
     {
         System.out.println(new App().greet("world"));
+        System.out.println(new App().greet("world"));
+        System.out.println(new App().greet("world"));
+        System.out.println(new App().greet("world"));
+        System.out.println(new App().greet("world"));
+        System.out.println(new App().greet("world"));
+        System.out.println(new App().greet("world"));
+        System.out.println(new App().greet("world"));
+        System.out.println(new App().greet("world"));
+        System.out.println(new App().greet("world"));
+        System.out.println(new App().greet("world"));
+        System.out.println(new App().greet("world"));
+        System.out.println(new App().greet("world"));
+        System.out.println(new App().greet("world"));
+        System.out.println(new App().greet("world"));
+        System.out.println(new App().greet("world"));
+        System.out.println(new App().greet("world"));
+        System.out.println(new App().greet("world"));
+        System.out.println(new App().greet("world"));
+        System.out.println(new App().greet("world"));
+        System.out.println(new App().greet("world"));
+        System.out.println(new App().greet("world"));
+        System.out.println(new App().greet("world"));
+        System.out.println(new App().greet("world"));
+        System.out.println(new App().greet("world"));
+        System.out.println(new App().greet("world"));
+        System.out.println(new App().greet("world"));
+        System.out.println(new App().greet("world"));
+        
     }
 
     public String greet(String name) {
